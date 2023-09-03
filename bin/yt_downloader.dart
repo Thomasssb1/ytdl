d1ea0039ee0a155684e043508599578ba40fca73
Thomasssb1 <thomas.beer04@outlook.com>
2023-09-03 17:19:07 +0100

    Interact with the cli

--- bin/yt_downloader.dart
+++ bin/yt_downloader.dart
@@ -1,5 +1,14 @@
+import 'package:args/args.dart';
 import 'package:yt_downloader/yt_downloader.dart' as yt_downloader;
+import 'package:yt_downloader/cmd.dart';
+import 'dart:io';
 
 void main(List<String> arguments) async{
-  await yt_downloader.downloadVideo("CH50zuS8DD0");
+  ArgResults? results = parse(arguments);
+  if (results == null){
+    print("Something went wrong. Arguments could not be parsed. Please try again.");
+    exit(0);
+  }else{
+    await yt_downloader.downloadVideo(results['args']['video']);
+  }
 }
