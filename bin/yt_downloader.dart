f4ea679d6ce6feb863666cf084f925bbdb1f60bf
Thomasssb1 <thomas.beer04@outlook.com>
2023-09-03 17:03:59 +0100

    first commit

---
+++ bin/yt_downloader.dart
@@ -0,0 +1,5 @@
+import 'package:yt_downloader/yt_downloader.dart' as yt_downloader;
+
+void main(List<String> arguments) async{
+  await yt_downloader.downloadVideo("CH50zuS8DD0");
+}
---
+++ lib/yt_downloader.dart
@@ -0,0 +1,17 @@
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import 'dart:io';
+
+downloadVideo(String id) async{
+  print("Downloading video from youtube that will play in the background. The video being downloaded is https://www.youtube.com/watch?v=n_Dv4JMiwK8");
+    var yt = YoutubeExplode();
+    var manifest = await yt.videos.streams.getManifest(id);
+    var streamInfo =  manifest.videoOnly.sortByVideoQuality().first;
+    var stream = yt.videos.streamsClient.get(streamInfo);
+    await File("results\\video1.mp4").create().then((File file) async{;
+    var fileStream = file.openWrite();
+    await stream.pipe(fileStream);
+
+    await fileStream.flush();
+    await fileStream.close();
+    });
+}
