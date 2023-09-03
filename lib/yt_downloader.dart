e73a2c69aea9ad6de2c3ab361f7a31d73233a5c2
Thomasssb1 <thomas.beer04@outlook.com>
2023-09-03 18:13:15 +0100

    Absolute path output

--- lib/yt_downloader.dart
+++ lib/yt_downloader.dart
@@ -1,15 +1,17 @@
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 import 'dart:io';
+import 'package:path/path.dart' as p;
 
 downloadVideo(String id, String output) async{
-  print("Downloading video from youtube that will play in the background. The video being downloaded is https://www.youtube.com/watch?v=$id");
+  print("Downloading video from youtube. The video being downloaded is https://www.youtube.com/watch?v=$id");
     var yt = YoutubeExplode();
     var manifest = await yt.videos.streams.getManifest(id);
     var streamInfo =  manifest.videoOnly.sortByVideoQuality().first;
     var stream = yt.videos.streamsClient.get(streamInfo);
     await File(output).create().then((File file) async{;
     var fileStream = file.openWrite();
-    await stream.pipe(fileStream).whenComplete(()=>print("Video download has completed. Check $output."));
+    
+    await stream.pipe(fileStream).whenComplete(()=>print("Video download has completed. Check ${(p.isAbsolute(output) ? output : Directory.current.path + r"\" + output)}."));
 
     await fileStream.flush();
     await fileStream.close();
