8c00c1a0eb2a1ae6aab6242b0dee180519f2be30
Thomasssb1 <thomas.beer04@outlook.com>
2023-09-08 02:09:07 +0100

    Rename file

--- bin/ytdl.dart
+++ bin/ytdl.dart
@@ -3,13 +3,14 @@ import 'package:yt_downloader/yt_downloader.dart' as yt_downloader;
 import 'package:yt_downloader/cmd.dart';
 import 'dart:io';
 
-void main(List<String> arguments) async{
+void main(List<String> arguments) async {
   ArgResults? results = parse(arguments);
-  if (results == null){
-    print("Something went wrong. Arguments could not be parsed. Please try again.");
+  if (results == null) {
+    print(
+        "Something went wrong. Arguments could not be parsed. Please try again.");
     exit(0);
-  }else{
+  } else {
     String id = yt_downloader.getVideoId(results['video']);
-    await yt_downloader.downloadVideo(id, results['output']);
+    await yt_downloader.downloadVideo(id, results['output'], results['type']);
   }
 }
